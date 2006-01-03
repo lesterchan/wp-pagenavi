d53f282b9f37854c08b3a73e72f00a114dcf82aa
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2006-01-03 13:38:24 +0000

    Adding wp-pagenavi 2.00
    git-svn-id: http://plugins.svn.wordpress.org/wp-pagenavi/trunk@4393 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pagenavi.php
@@ -0,0 +1,66 @@
+<?php
+/*
+Plugin Name: WP-PageNavi
+Plugin URI: http://www.lesterchan.net/portfolio/programming.php
+Description: Adds a more advanced page navigation to Wordpress.
+Version: 2.0
+Author: GaMerZ
+Author URI: http://www.lesterchan.net
+*/
+
+
+/*  Copyright 2005  Lester Chan  (email : gamerz84@hotmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+
+### Function: Page Navigation
+function wp_pagenavi($before=' ', $after=' ', $prelabel='&laquo;', $nxtlabel='&raquo;') {
+	global $request, $posts_per_page, $wpdb, $paged;
+	if (!is_single()) {
+		if (get_query_var('what_to_show') == 'posts') {
+			preg_match('#FROM\s(.*)\sGROUP BY#siU', $request, $matches);
+			$fromwhere = $matches[1];
+			$numposts = $wpdb->get_var("SELECT COUNT(DISTINCT ID) FROM $fromwhere");
+			$max_page = ceil($numposts /$posts_per_page);
+		} else {
+			$max_page = 999999;
+		}
+		if(empty($paged)) {
+			$paged = 1;
+		}
+		echo "$before Pages ($max_page): <b>";
+		if ($paged >= 4) {
+			echo '<a href="'.get_pagenum_link().'">&laquo; First</a> ... ';
+		}
+		previous_posts_link($prelabel);
+		for($i = $paged - 2 ; $i  <= $paged +2; $i++) {
+			if ($i >= 1 && $i <= $max_page) {
+				if($i == $paged) {
+					echo "[$i]";
+				} else {
+					echo ' <a href="'.get_pagenum_link($i).'">'.$i.'</a> ';
+				}
+			}
+		}
+		next_posts_link($nxtlabel, $max_page);
+		if (($paged+2) < ($max_page)) {
+			echo ' ... <a href="'.get_pagenum_link($max_page).'">Last &raquo;</a>';
+		}
+		echo "$after</b>";
+	}
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,13 @@
+-> Page Navigation Plugin For WordPress 2.0
+--------------------------------------------------
+Author	-> Lester 'GaMerZ' Chan
+Email	-> gamerz84@hotmail.com
+Website	-> http://www.lesterchan.net/
+Demo	-> http://www.lesterchan.net/blogs
+Version	-> 2.0
+Updated	-> 1st January 2006
+--------------------------------------------------
+
+// Version 2.00 (01-01-2006)
+- NEW: Compatible With WordPress 2.0
+- FIXED: Space Issues
