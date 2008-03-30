3744f167bd425788a5d128f476ce49030b425275
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-03-30 10:02:52 +0000

    so that it looks nice on WP2.5
    git-svn-id: http://plugins.svn.wordpress.org/wp-pagenavi/trunk@36833 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pagenavi-options.php
+++ pagenavi-options.php
@@ -105,108 +105,102 @@ switch($mode) {
 <form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>"> 
 <div class="wrap"> 
 	<h2><?php _e('Page Navigation Options', 'wp-pagenavi'); ?></h2>
+	<h3><?php _e('Page Navigation Text', 'wp-pagenavi'); ?></h3>
+	<table class="form-table">
+		<tr>
+			<th scope="row" valign="top"><?php _e('Text For Number Of Pages', 'wp-pagenavi'); ?></th>
+			<td>
+				<input type="text" name="pagenavi_pages_text" value="<?php echo stripslashes($pagenavi_options['pages_text']); ?>" size="50" /><br />
+				%CURRENT_PAGE% - <?php _e('The current page number.', 'wp-pagenavi'); ?><br />
+				%TOTAL_PAGES% - <?php _e('The total number of pages.', 'wp-pagenavi'); ?>
+			</td>
+		</tr>
+		<tr>
+			<th scope="row" valign="top"><?php _e('Text For Current Page', 'wp-pagenavi'); ?></th>
+			<td>
+				<input type="text" name="pagenavi_current_text" value="<?php echo stripslashes(htmlspecialchars($pagenavi_options['current_text'])); ?>" size="30" /><br />
+				%PAGE_NUMBER% - <?php _e('The page number.', 'wp-pagenavi'); ?><br />
+			</td>
+		</tr>
+		<tr>
+			<th scope="row" valign="top"><?php _e('Text For Page', 'wp-pagenavi'); ?></th>
+			<td>
+				<input type="text" name="pagenavi_page_text" value="<?php echo stripslashes(htmlspecialchars($pagenavi_options['page_text'])); ?>" size="30" /><br />
+				%PAGE_NUMBER% - <?php _e('The page number.', 'wp-pagenavi'); ?><br />
+			</td>
+		</tr>
+		<tr>
+			<th scope="row" valign="top"><?php _e('Text For First Post', 'wp-pagenavi'); ?></th>
+			<td>
+				<input type="text" name="pagenavi_first_text" value="<?php echo stripslashes(htmlspecialchars($pagenavi_options['first_text'])); ?>" size="30" /><br />
+				%TOTAL_PAGES% - <?php _e('The total number of pages.', 'wp-pagenavi'); ?>
+			</td>
+		</tr>
+		<tr>
+			<th scope="row" valign="top"><?php _e('Text For Last Post', 'wp-pagenavi'); ?></th>
+			<td>
+				<input type="text" name="pagenavi_last_text" value="<?php echo stripslashes(htmlspecialchars($pagenavi_options['last_text'])); ?>" size="30" /><br />
+				%TOTAL_PAGES% - <?php _e('The total number of pages.', 'wp-pagenavi'); ?>
+			</td>
+		</tr>
+		<tr>
+			<th scope="row" valign="top"><?php _e('Text For Next Post', 'wp-pagenavi'); ?></th>
+			<td>
+				<input type="text" name="pagenavi_next_text" value="<?php echo stripslashes(htmlspecialchars($pagenavi_options['next_text'])); ?>" size="30" />
+			</td>
+		</tr>
+		<tr>
+			<th scope="row" valign="top"><?php _e('Text For Previous Post', 'wp-pagenavi'); ?></th>
+			<td>
+				<input type="text" name="pagenavi_prev_text" value="<?php echo stripslashes(htmlspecialchars($pagenavi_options['prev_text'])); ?>" size="30" />
+			</td>
+		</tr>
+		<tr>
+			<th scope="row" valign="top"><?php _e('Text For Next ...', 'wp-pagenavi'); ?></th>
+			<td>
+				<input type="text" name="pagenavi_dotright_text" value="<?php echo stripslashes(htmlspecialchars($pagenavi_options['dotright_text'])); ?>" size="30" />
+			</td>
+		</tr>
+		<tr>
+			<th scope="row" valign="top"><?php _e('Text For Previous ...', 'wp-pagenavi'); ?></th>
+			<td>
+				<input type="text" name="pagenavi_dotleft_text" value="<?php echo stripslashes(htmlspecialchars($pagenavi_options['dotright_text'])); ?>" size="30" />
+			</td>
+		</tr>
+	</table>
+	<h3><?php _e('Page Navigation Options', 'wp-pagenavi'); ?></h3>
+	<table class="form-table">
+		<tr>
+			<th scope="row" valign="top"><?php _e('Page Navigation Style', 'wp-pagenavi'); ?></th>
+			<td>
+				<select name="pagenavi_style" size="1">
+					<option value="1"<?php selected('1', $pagenavi_options['style']); ?>><?php _e('Normal', 'wp-pagenavi'); ?></option>
+					<option value="2"<?php selected('2', $pagenavi_options['style']); ?>><?php _e('Drop Down List', 'wp-pagenavi'); ?></option>
+				</select>
+			</td> 
+		</tr>
+		<tr>
+			<th scope="row" valign="top"><?php _e('Number Of Pages To Show?', 'wp-pagenavi'); ?></th>
+			<td>
+				<input type="text" name="pagenavi_num_pages" value="<?php echo stripslashes($pagenavi_options['num_pages']); ?>" size="4" />
+			</td>
+		</tr>
+		<tr>
+			<th scope="row" valign="top"><?php _e('Always Show Page Navigation?', 'wp-pagenavi'); ?></th>
+			<td>
+				<select name="pagenavi_always_show" size="1">
+					<option value="1"<?php selected('1', $pagenavi_options['always_show']); ?>><?php _e('Yes', 'wp-pagenavi'); ?></option>
+					<option value="0"<?php selected('0', $pagenavi_options['always_show']); ?>><?php _e('No', 'wp-pagenavi'); ?></option>
+				</select>
+			</td> 
+		</tr>
+	</table>
 	<p class="submit">
-		<input type="submit" name="Submit" class="button" value="<?php _e('Update Options &raquo;', 'wp-pagenavi'); ?>" />
-	</p>
-	<fieldset class="options">
-		<legend><?php _e('Page Navigation Text', 'wp-pagenavi'); ?></legend>
-		<table width="100%"  border="0" cellspacing="3" cellpadding="3">
-			<tr valign="top">
-				<th align="left" width="30%"><?php _e('Text For Number Of Pages', 'wp-pagenavi'); ?></th>
-				<td align="left">
-					<input type="text" name="pagenavi_pages_text" value="<?php echo stripslashes($pagenavi_options['pages_text']); ?>" size="50" /><br />
-					%CURRENT_PAGE% - <?php _e('The current page number.', 'wp-pagenavi'); ?><br />
-					%TOTAL_PAGES% - <?php _e('The total number of pages.', 'wp-pagenavi'); ?>
-				</td>
-			</tr>
-			<tr valign="top">
-				<th align="left" width="30%"><?php _e('Text For Current Page', 'wp-pagenavi'); ?></th>
-				<td align="left">
-					<input type="text" name="pagenavi_current_text" value="<?php echo stripslashes(htmlspecialchars($pagenavi_options['current_text'])); ?>" size="30" /><br />
-					%PAGE_NUMBER% - <?php _e('The page number.', 'wp-pagenavi'); ?><br />
-				</td>
-			</tr>
-			<tr valign="top">
-				<th align="left" width="30%"><?php _e('Text For Page', 'wp-pagenavi'); ?></th>
-				<td align="left">
-					<input type="text" name="pagenavi_page_text" value="<?php echo stripslashes(htmlspecialchars($pagenavi_options['page_text'])); ?>" size="30" /><br />
-					%PAGE_NUMBER% - <?php _e('The page number.', 'wp-pagenavi'); ?><br />
-				</td>
-			</tr>
-			<tr valign="top">
-				<th align="left" width="30%"><?php _e('Text For First Post', 'wp-pagenavi'); ?></th>
-				<td align="left">
-					<input type="text" name="pagenavi_first_text" value="<?php echo stripslashes(htmlspecialchars($pagenavi_options['first_text'])); ?>" size="30" /><br />
-					%TOTAL_PAGES% - <?php _e('The total number of pages.', 'wp-pagenavi'); ?>
-				</td>
-			</tr>
-			<tr valign="top">
-				<th align="left" width="30%"><?php _e('Text For Last Post', 'wp-pagenavi'); ?></th>
-				<td align="left">
-					<input type="text" name="pagenavi_last_text" value="<?php echo stripslashes(htmlspecialchars($pagenavi_options['last_text'])); ?>" size="30" /><br />
-					%TOTAL_PAGES% - <?php _e('The total number of pages.', 'wp-pagenavi'); ?>
-				</td>
-			</tr>
-			<tr valign="top">
-				<th align="left" width="30%"><?php _e('Text For Next Post', 'wp-pagenavi'); ?></th>
-				<td align="left">
-					<input type="text" name="pagenavi_next_text" value="<?php echo stripslashes(htmlspecialchars($pagenavi_options['next_text'])); ?>" size="30" />
-				</td>
-			</tr>
-			<tr valign="top">
-				<th align="left" width="30%"><?php _e('Text For Previous Post', 'wp-pagenavi'); ?></th>
-				<td align="left">
-					<input type="text" name="pagenavi_prev_text" value="<?php echo stripslashes(htmlspecialchars($pagenavi_options['prev_text'])); ?>" size="30" />
-				</td>
-			</tr>
-			<tr valign="top">
-				<th align="left" width="30%"><?php _e('Text For Next ...', 'wp-pagenavi'); ?></th>
-				<td align="left">
-					<input type="text" name="pagenavi_dotright_text" value="<?php echo stripslashes(htmlspecialchars($pagenavi_options['dotright_text'])); ?>" size="30" />
-				</td>
-			</tr>
-			<tr valign="top">
-				<th align="left" width="30%"><?php _e('Text For Previous ...', 'wp-pagenavi'); ?></th>
-				<td align="left">
-					<input type="text" name="pagenavi_dotleft_text" value="<?php echo stripslashes(htmlspecialchars($pagenavi_options['dotright_text'])); ?>" size="30" />
-				</td>
-			</tr>
-		</table>
-	</fieldset>
-	<fieldset class="options">
-		<legend><?php _e('Page Navigation Options', 'wp-pagenavi'); ?></legend>
-		<table width="100%"  border="0" cellspacing="3" cellpadding="3">
-			<tr valign="top"> 
-				<th align="left" width="30%"><?php _e('Page Navigation Style', 'wp-pagenavi'); ?></th>
-				<td align="left">
-					<select name="pagenavi_style" size="1">
-						<option value="1"<?php selected('1', $pagenavi_options['style']); ?>><?php _e('Normal', 'wp-pagenavi'); ?></option>
-						<option value="2"<?php selected('2', $pagenavi_options['style']); ?>><?php _e('Drop Down List', 'wp-pagenavi'); ?></option>
-					</select>
-				</td> 
-			</tr>
-			 <tr valign="top">
-				<th align="left" width="30%"><?php _e('Number Of Pages To Show?', 'wp-pagenavi'); ?></th>
-				<td align="left">
-					<input type="text" name="pagenavi_num_pages" value="<?php echo stripslashes($pagenavi_options['num_pages']); ?>" size="4" />
-				</td>
-			</tr>
-			<tr valign="top"> 
-				<th align="left" width="30%"><?php _e('Always Show Page Navigation?', 'wp-pagenavi'); ?></th>
-				<td align="left">
-					<select name="pagenavi_always_show" size="1">
-						<option value="1"<?php selected('1', $pagenavi_options['always_show']); ?>><?php _e('Yes', 'wp-pagenavi'); ?></option>
-						<option value="0"<?php selected('0', $pagenavi_options['always_show']); ?>><?php _e('No', 'wp-pagenavi'); ?></option>
-					</select>
-				</td> 
-			</tr>
-		</table>
-	</fieldset>
-	<p class="submit">
-		<input type="submit" name="Submit" class="button" value="<?php _e('Update Options &raquo;', 'wp-pagenavi'); ?>" />
+		<input type="submit" name="Submit" class="button" value="<?php _e('Save Changes', 'wp-pagenavi'); ?>" />
 	</p>
 </div>
 </form> 
+<p>&nbsp;</p>
 
 <!-- Uninstall WP-PageNavi -->
 <form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>"> 
@@ -222,12 +216,14 @@ switch($mode) {
 	<p style="text-align: left; color: red">
 		<strong><?php _e('The following WordPress Options will be DELETED:', 'wp-pagenavi'); ?></strong><br />
 	</p>
-	<table width="70%"  border="0" cellspacing="3" cellpadding="3">
-		<tr class="thead">
-			<td align="center"><strong><?php _e('WordPress Options', 'wp-pagenavi'); ?></strong></td>
-		</tr>
+	<table class="widefat">
+		<thead>
+			<tr>
+				<th><?php _e('WordPress Options', 'wp-pagenavi'); ?></th>
+			</tr>
+		</thead>
 		<tr>
-			<td valign="top" style="background-color: #eee;">
+			<td valign="top">
 				<ol>
 				<?php
 					foreach($pagenavi_settings as $settings) {
