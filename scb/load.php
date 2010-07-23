67de6038ef727978537c61a64f3693d98c7e1f44
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-23 13:56:53 +0000

    add AL i18n. Props Visar Uruqi
    git-svn-id: http://plugins.svn.wordpress.org/wp-pagenavi/trunk@266305 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- scb/load.php
+++ scb/load.php
@@ -1,8 +1,10 @@
 <?php
 
-$GLOBALS['_scb_data'] = array( 21, __FILE__, array( 'scbUtil', 'scbOptions', 'scbForms', 'scbTable',
+$GLOBALS['_scb_data'] = array( 23, __FILE__, array(
+	'scbUtil', 'scbOptions', 'scbForms', 'scbTable',
 	'scbWidget', 'scbAdminPage', 'scbBoxesPage',
-	'scbQuery', 'scbRewrite', 'scbCron', ) );
+	'scbQueryManipulation', 'scbRewrite', 'scbCron',
+) );
 
 if ( !class_exists( 'scbLoad4' ) ) :
 class scbLoad4 {
@@ -22,7 +24,7 @@ class scbLoad4 {
 		if ( !empty( $callback ) ) {
 			self::$callbacks[$file] = $callback;
 
-			add_action( 'activated_plugin',  array( __CLASS__, 'delayed_activation' ) );
+			add_action( 'activate_plugin',  array( __CLASS__, 'delayed_activation' ) );
 		}
 
 		// TODO: don't load when activating a plugin ?
@@ -35,42 +37,13 @@ class scbLoad4 {
 		if ( '.' == $plugin_dir )
 			return;
 
-		$found = false;
 		foreach ( self::$callbacks as $file => $callback )
 			if ( plugin_basename( dirname( dirname( $file ) ) ) == $plugin_dir ) {
-				$found = true;
+				self::load( false );
+				call_user_func( $callback );
+				do_action( 'scb_activation_' . $plugin );
 				break;
 			}
-		if ( !$found )
-			return;
-
-		self::load( false );
-		call_user_func( $callback );
-
-		if ( is_plugin_active_for_network( $plugin ) ) {
-			// network wide activation
-			foreach ( self::get_active_blogs() as $blog_id ) {
-				switch_to_blog( $blog_id );
-				do_action( 'scb_activation_' . $plugin );
-			}
-			restore_current_blog();
-		}
-		else {
-			// regular activation
-			do_action( 'scb_activation_' . $plugin );
-		}
-	}
-
-	// TODO: bail activation if more than X blogs
-	private static function get_active_blogs() {
-		global $wpdb;
-
-		return $wpdb->get_col( "
-			SELECT blog_id
-			FROM $wpdb->blogs
-			WHERE site_id = '{$wpdb->siteid}'
-			AND deleted = 0
-		" );
 	}
 
 	static function load( $do_callbacks = true ) {
