7d3f97aa2897872841cbcbb26f5ea15d2a5d8a82
scribu <mail@scribu.net>
2011-03-26 17:50:48 +0200

    bump version to 2.75-alpha2

--- wp-pagenavi.php
+++ wp-pagenavi.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WP-PageNavi
-Version: 2.75-alpha
+Version: 2.75-alpha2
 Description: Adds a more advanced paging navigation to your WordPress blog
 Author: Lester 'GaMerZ' Chan & scribu
 Plugin URI: http://wordpress.org/extend/plugins/wp-pagenavi/
