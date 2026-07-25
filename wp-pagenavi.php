bd07bd7aaaec014e21672ed2914788fc18c00a54
Lester Chan <lesterchan@gmail.com>
2026-07-25 10:22:38 +0800

    Add GPL copyright header

--- wp-pagenavi.php
+++ wp-pagenavi.php
@@ -9,6 +9,24 @@ Author URI: https://lesterchan.net
 Text Domain: wp-pagenavi
 */
 
+/*
+	Copyright 2026  Lester Chan  (email : lesterchan@gmail.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 require_once __DIR__ . '/scb/load.php';
 
 function _pagenavi_init() {
